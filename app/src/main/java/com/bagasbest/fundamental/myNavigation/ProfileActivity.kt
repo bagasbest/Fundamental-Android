1125dc6d3939f901f725f4df58ab6c16315cdfc7
bagasbest <bagasbest80@gmail.com>
2021-03-09 08:03:42 +0700

    Navigation

---
+++ app/src/main/java/com/bagasbest/fundamental/myNavigation/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.bagasbest.fundamental.myNavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bagasbest.fundamental.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
